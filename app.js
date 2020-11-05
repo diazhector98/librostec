7e326e1ef33cc92ada2749087796882b7fdb1769
diazhector98 <diazhector98@gmail.com>
2020-11-05 15:13:34 -0600

    adding gitignore

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://diazhector98:Govosin98!@cluster0.oqgjb.mongodb.net/librostec?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 const port = 3000
+
+client.connect(err => {
+  const collection = client.db("librostec").collection("users")
+  collection.findOne({}, function(err, result) {
+    if (err) throw err;
+    console.log(result);
+  });;
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.use(express.static("public"))
 
