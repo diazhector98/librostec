e65e317b1679ff5ec0adbd8681988c1c526c3b58
diazhector98 <diazhector98@gmail.com>
2020-11-06 09:55:02 -0600

    adding book search endpoint

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
-const cors = require('cors')
+const axios = require('axios')
+const cors = require('cors');
+const { default: Axios } = require('axios');
+const { response } = require('express');
 const MongoClient = require('mongodb').MongoClient;
 
 dotenv.config();
@@ -28,6 +31,7 @@ app.post('/user', (request, response) => {
     email
   }
 
+  //Nombre del autor, categoría, fechas de publicación,imagen
   const collection = database.collection("users")
   collection.insert(user, (error, result) => {
     if (error) {
@@ -36,8 +40,65 @@ app.post('/user', (request, response) => {
     response.send(user)
   })
 })
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+// https://developers.google.com/books/docs/v1/getting_started
+// AIzaSyBG1fVsps6ZrjXH2liIVThgAU-Zt5qv5qk
+app.get('/books', (request, response) => {
+  const {
+    textQuery
+  } = request.query
+
+  const apiKey = "AIzaSyBG1fVsps6ZrjXH2liIVThgAU-Zt5qv5qk"
+  const maxResults = 40
+  const searchBooksEndpoint = `https://www.googleapis.com/books/v1/volumes?q=${textQuery.split(" ").join("+")}&key=${apiKey}&maxResults=${maxResults}`
+  axios.get(searchBooksEndpoint).then((result) => {
+    const {totalItems, items} = result.data
+    let filteredItems = []
+    let bookIndex = 0;
+
+    while (bookIndex < maxResults) {
+      
+      const item = items[bookIndex]
+
+      if (!item) {
+        bookIndex++
+        continue
+      } 
+      const volumeInfo = item.volumeInfo
+      const {
+        title,
+        subtitle,
+        authors,
+        publishedDate,
+        description,
+        pageCount,
+        categories,
+        imageLinks,
+        averageRating
+      } = volumeInfo
+
+      filteredItems.push({
+        title,
+        subtitle,
+        authors,
+        publishedDate,
+        description,
+        pageCount,
+        categories,
+        imageLinks,
+        averageRating
+      })
+      bookIndex++
+    }
+
+    response.send({
+      totalItemsFetched: totalItems,
+      itemsReturned: filteredItems.length,
+      books: filteredItems
+    })
+
+  })
+
 })
 
 app.listen(process.env.PORT || 3000, 
